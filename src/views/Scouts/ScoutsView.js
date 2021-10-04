67da25545e55a0c5d91c706db431a58de676c1d3
Oliver Bullock <ojbullock@googlemail.com>
2021-10-04 21:21:29 +0100

    Updated Scouts to include a header for the Scouts page

--- src/views/Scouts/ScoutsView.js
+++ src/views/Scouts/ScoutsView.js
@@ -4,21 +4,30 @@ import IdeasByBadge from './sections/IdeasByBadge'
 import AboutMe from './sections/AboutMe';
 import Usefulresources from './sections/UsefulResources';
 
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+import MenuAppBar from '../../components/MenuAppBar';
+
 const ScoutsView = () => {
     return (
-        <div>
-            <ul>
-                <li>
-                    <AboutMe/>
-                </li>
-                <li>
-                    <IdeasByBadge/>
-                </li>
-                <li>
-                    <Usefulresources/>
-                </li>
-            </ul>
-        </div>
+        <Container maxWidth="lg">
+            <MenuAppBar title="My Portfolio" />
+            <div className='page-body'>
+                <Avatar alt='Oliver Bullock' src="static/images/oliver-bullock-avatar.png" />
+
+                <ul>
+                    <li>
+                        <AboutMe />
+                    </li>
+                    <li>
+                        <IdeasByBadge />
+                    </li>
+                    <li>
+                        <Usefulresources />
+                    </li>
+                </ul>
+            </div>
+        </Container>
     )
 }
 
