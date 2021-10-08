a33092b3752d5977f59ef0ca6c2a38cff71d903b
Oliver Bullock <kinetic-coder@users.noreply.github.com>
2021-10-08 07:35:48 +0100

    Created a new page for the boys network

---
+++ src/views/TheBoysNetworkView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TheBoysNetworkView = () => {
+    return (
+         <>
+             The boys newtwork
+         </>
+    );
+}
+
+export default TheBoysNetworkView;
