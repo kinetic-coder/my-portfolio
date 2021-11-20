e55f88b9cf8b702d607f20835bd09b75b9a66d07
Oliver Bullock <ojbullock@googlemail.com>
2021-11-20 14:10:54 +0000

    Added boys network page

--- src/views/TheBoysNetworkView/TheBoysNetworkView.js
+++ src/views/TheBoysNetworkView/TheBoysNetworkView.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
+import { Container, Avatar } from '@mui/material';
+import MenuAppBar from '../../components/MenuAppBar';
+
+
 const TheBoysNetworkView = () => {
     return (
-         <>
-             The boys newtwork
-         </>
+        <Container maxWidth="lg">
+        <MenuAppBar title="My Portfolio" />
+        <div className='page-body'>
+            <Avatar alt='Oliver Bullock' src="static/images/oliver-bullock-avatar.png" />
+
+            {/* <ul>
+                <li>
+                    <AboutMe />
+                </li>
+                <li>
+                    <IdeasByBadge />
+                </li>
+                <li>
+                    <Usefulresources />
+                </li>
+            </ul> */}
+        </div>
+    </Container>
     );
 }
 
